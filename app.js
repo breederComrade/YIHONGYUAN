0a8ca9a6b7a3e9fb5bb476be8cb16121fe6f9139
王俊 <1871015627@qq.com>
2018-06-26 10:39:28 +0800

    add basic file

---
+++ app.js
@@ -0,0 +1,20 @@
+/*
+ * 入库文件 
+ */
+const express = require('express'),
+	app = module.exports = express(),
+	router = express.Router(),
+	path = require('path');
+
+app.use('/',(req,res)=>{
+   
+});
+
+const web = require('./')
+app.use('/web',)
+
+
+
+
+
+
